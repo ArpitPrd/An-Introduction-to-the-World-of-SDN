2e6e777c4c33f972be209bdb42b561170e207232
ArpitPrd <prd.arpit@gmail.com>
2025-10-09 21:48:15 +0530

    updated part2

--- part2/p2_l2spf.py
+++ part2/p2_l2spf.py
@@ -8,36 +8,34 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, ether_types, arp
 from ryu.topology import event
 from ryu.topology.api import get_switch
-# **STEP 1: Import the Switches class**
+# **FIX 1: Import the Switches class**
 from ryu.topology.switches import Switches
 
 import networkx as nx
 
 class L2SPF(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    # **STEP 2: Add the _CONTEXTS dictionary to declare dependency**
+    # **FIX 2: Add the _CONTEXTS dictionary to declare the dependency**
     _CONTEXTS = {'switches': Switches}
 
     def __init__(self, *args, **kwargs):
         super(L2SPF, self).__init__(*args, **kwargs)
-        # **STEP 3: Get the switches instance from the context**
+        # Get the switches instance from the context
         self.switches = kwargs['switches']
         
         self.mac_to_port = {}
         
+        # This part of your code was correct
         try:
             with open("config.json", 'r') as f:
                 self.config = json.load(f)
-        except (IOError, ValueError) as e:
-            self.logger.error("Error loading config.json: %s. Using defaults.", e)
-            self.config = {}
+        except (IOError, ValueError):
+            self.config = {} # Default if file not found or invalid
 
         self.weight_matrix = self.config.get("weight_matrix", [])
         self.ecmp = self.config.get("ecmp", False)
         self.graph = nx.DiGraph()
         self.logger.info("L2SPF Controller Started. ECMP is %s.", "enabled" if self.ecmp else "disabled")
-        # Define the number of links expected in the topology
-        self.EXPECTED_LINKS = 8
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -49,8 +47,6 @@ class L2SPF(app_manager.RyuApp):
         self.add_flow(datapath, 0, match, actions)
         self.logger.info("Switch %s connected.", datapath.id)
 
-    # Note: EventSwitchEnter is no longer needed as we get switch data from EventLinkAdd
-    
     @set_ev_cls(event.EventLinkAdd)
     def handle_link_add(self, ev):
         src_dpid = ev.link.src.dpid
@@ -58,18 +54,21 @@ class L2SPF(app_manager.RyuApp):
         src_port = ev.link.src.port_no
         dst_port = ev.link.dst.port_no
 
-        # Add nodes if they don't already exist
+        # Your logic for adding nodes and edges was correct
         self.graph.add_node(src_dpid)
         self.graph.add_node(dst_dpid)
 
         if self.weight_matrix and max(src_dpid, dst_dpid) <= len(self.weight_matrix):
             edge_cost = self.weight_matrix[src_dpid - 1][dst_dpid - 1]
         else:
-            edge_cost = 1 # Default cost if matrix is not configured
+            edge_cost = 1
         
         self.graph.add_edge(src_dpid, dst_dpid, port=src_port, weight=edge_cost)
         self.graph.add_edge(dst_dpid, src_dpid, port=dst_port, weight=edge_cost)
-        self.logger.info("Added link from %s to %s with cost %s", src_dpid, dst_dpid, edge_cost)
+        self.logger.info("Added link: %s <-> %s", src_dpid, dst_dpid)
+        
+        # **FIX 3 (YOUR IDEA): Add diagnostic printing**
+        self.logger.info("Current Graph Edges: %s", self.graph.edges())
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
@@ -79,7 +78,6 @@ class L2SPF(app_manager.RyuApp):
         datapath.send_msg(mod)
 
     def select_route(self, routes):
-        """Selects a route based on the ECMP setting."""
         if self.ecmp and len(routes) > 1:
             return random.choice(routes)
         return routes[0]
@@ -100,32 +98,18 @@ class L2SPF(app_manager.RyuApp):
 
         dst = eth.dst
         src = eth.src
-        # Using your original naming convention
         src_switch_id = datapath.id
-
-
-        # Handle ARP packets specifically
-        arp_pkt = pkt.get_protocol(arp.arp)
-        if arp_pkt:
-            self.logger.info("ARP packet received from %s. Flooding.", src)
-            self.flood_packet(datapath, msg)
-            return
 
         self.mac_to_port[src] = (src_switch_id, in_port)
 
+        arp_pkt = pkt.get_protocol(arp.arp)
+        if arp_pkt:
+            self.flood_packet(datapath, msg)
+            return
+
         if dst in self.mac_to_port:
-            # Using your original naming convention
             dst_switch_id, dst_port = self.mac_to_port[dst]
             
-            # *** NEW STABILITY CHECK ***
-            # Do not attempt to calculate a path until the topology is fully discovered.
-            # The graph will have 2 directed edges for each undirected link.
-            # if len(self.graph.edges) < self.EXPECTED_LINKS * 2:
-            #     self.logger.warning("Topology not fully discovered yet (%s/%s edges). Flooding packet.", 
-            #                       len(self.graph.edges), self.EXPECTED_LINKS * 2)
-            #     self.flood_packet(datapath, msg)
-            #     return
-
             if src_switch_id == dst_switch_id:
                 actions = [parser.OFPActionOutput(dst_port)]
                 match = parser.OFPMatch(eth_dst=dst)
@@ -140,8 +124,7 @@ class L2SPF(app_manager.RyuApp):
                 selected_path = self.select_route(paths)
                 self.logger.info("Selected path for %s -> %s: %s", src, dst, selected_path)
 
-                # Install rules on all switches in the path
-                # Using your original naming convention
+                # Proactively install rules on all switches in the path
                 for i in range(len(selected_path) - 1):
                     this_switch = selected_path[i]
                     next_switch = selected_path[i+1]
@@ -164,10 +147,11 @@ class L2SPF(app_manager.RyuApp):
                 self.send_packet_out(datapath, msg, [parser.OFPActionOutput(first_hop_port)])
 
             except nx.NetworkXNoPath:
-                self.logger.warning("No path from switch %s to %s. Flooding.", src_switch_id, dst_switch_id)
                 self.flood_packet(datapath, msg)
         else:
             self.flood_packet(datapath, msg)
+
+    # **FIX 4: Removed the old, conflicting logic from the end of the function**
 
     def flood_packet(self, datapath, msg):
         ofproto = datapath.ofproto
@@ -183,3 +167,4 @@ class L2SPF(app_manager.RyuApp):
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
